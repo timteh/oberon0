3c2c984d6e2399a46f20440242f855a419a28a01
WOOSTER\Timothy <timothytehsw@gmail.com>
2017-12-28 08:40:08 +0800

    Porting checkpoint.

--- Util.py
+++ Util.py
@@ -14,5 +14,30 @@ class Constant:
     def getValue(self):
         return self.__const
 
+# Predeclared procedures of component pascal
 def NEW(pointer):
     pointer.m_value = pointer.m_pValue()
+
+def INC(var, increment = None):
+    if increment is None:
+        var.m_value += 1
+    else:
+        var.m_value += increment
+
+def SHORT(var):
+    pass
+
+def EXCL(Set, element):
+    Set.m_value.discard(element)
+
+def INCL(Set, element):
+    Set.m_value.add(element)
+
+def ASH(var, shiftPos):
+    var = var << shiftPos
+
+def DEC(operand, decrement = None):
+    if decrement is None:
+        operand.m_value -= 1
+    else:
+        operand.m_value -= decrement
